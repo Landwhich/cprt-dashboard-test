46e99e0cb5e1b65d6b74bc5b4cbd8570b3202b83
Will <will.f.richards@gmail.com>
2024-11-18 15:11:11 -0500

    took an embarassingly long time to figure out my folder structure

---
+++ src/app/components/topics/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/joystick.module.css'
+
+export default function Joy() {
+    return(
+        <div 
+            className={styles.main}
+        >
+            hey gang
+        </div>
+    );
+}
