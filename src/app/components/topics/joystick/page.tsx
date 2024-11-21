77209901c6638ff953d0110803a19da25c3fd991
Will Richards <will.f.richards@gmail.com>
2024-11-21 14:45:02 -0500

    fix issue with ros handler - laptop dying :(

--- src/app/components/topics/joystick/page.tsx
+++ src/app/components/topics/joystick/page.tsx
@@ -1,13 +1,23 @@
 import styles from '../../styles/joystick.module.css'
+import { ros } from '../ros/ros-handler'
 
-//import circle from '../../../../server/joystick/joystick.js'
+var listener = new ros.Topic({
+    ros : ros,
+    name : '/listener',
+    messageType : 'std_msgs/String'
+  });
+
+  listener.subscribe((message) => {
+    console.log('Received message on ' + listener.name + ': ' + message.data);
+    listener.unsubscribe();
+  });
 
 export default function Joy() {
     return(
-        <div 
-            className={styles.main}
-        >
-            hey gang
+        <div>
+            <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/eventemitter2@6.4.9/lib/eventemitter2.min.js"></script>
+            <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/roslib@1/build/roslib.min.js"></script>
+            Joystick widget
         </div>
     );
 }
--- src/app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page(){
-  return <h1>Hey gang</h1>
-}
