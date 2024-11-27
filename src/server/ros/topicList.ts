bf6d343bfb7ede61f231beb625c06aced9d21b74
landwhich <will.f.richards@gmail.com>
2024-11-26 19:39:37 -0500

    topics are being displayed and rendered through an array. addiiton of a topic list

---
+++ src/server/ros/topicList.ts
@@ -0,0 +1,9 @@
+import Joy from '@/app/components/topics/joystick/page'
+import Map from '@/app/components/topics/map/page'
+
+const topicList = [
+    {component : Joy, path : "joystick"},
+    {component : Map, path : "map"}
+];
+
+export default topicList
